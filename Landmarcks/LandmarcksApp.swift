499225c51ad16916b5ad069842212e62662c19fb
nataliadpizzol <nataliadpizzol@gmail.com>
2023-03-28 13:52:28 -0300

    Initial Commit

---
+++ Landmarcks/LandmarcksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarcksApp.swift
+//  Landmarcks
+//
+//  Created by Natalia Dal Pizzol on 28/03/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarcksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
